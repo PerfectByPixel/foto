2c6a3084f3f4f39b4c84c6bcca5c96967a14d199
PerfectByPixel <140796026+PerfectByPixel@users.noreply.github.com>
2023-11-13 19:33:13 +0100

    Add files via upload

--- contact-form.php
+++ contact-form.php
@@ -13,40 +13,9 @@ if( ! empty( $_POST['email'] ) ) {
 	// Email Subject
 	$subject = 'Connect Form';
 
-	// Google reCaptcha V3 Key
-	$grecaptchav3_secret_key = '';
-
-	$from 	= $_POST['email'];
-	$name 	= isset( $_POST['name'] ) ? $_POST['name'] : '';
-	// $phone 	= isset( $_POST['phone'] ) ? $_POST['phone'] : '';
-	$comment= isset( $_POST['comment'] ) ? $_POST['comment'] : '';
-	
-	if( ! empty( $grecaptchav3_secret_key ) && ! empty( $_POST['g-recaptcha-response'] ) ) {
-
-		$token = $_POST['g-recaptcha-response'];
-
-		// call curl to POST request
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query( array( 'secret' => $grecaptchav3_secret_key, 'response' => $token ) ) );
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$response = curl_exec($ch);
-		curl_close($ch);
-		$arrResponse = json_decode($response, true);
-
-		// verify the response
-		if( isset( $_POST['action'] ) && ! ( isset( $arrResponse['success'] ) && $arrResponse['success'] == '1' && $arrResponse['action'] == $_POST['action'] && $arrResponse['score'] >= 0.5 ) ) {
-
-			echo '{ "alert": "alert-danger", "message": "Your message could not been sent due to invalid reCaptcha!" }';
-			die;
-
-		} else if( ! isset( $_POST['action'] ) && ! ( isset( $arrResponse['success'] ) && $arrResponse['success'] == '1' ) ) {
-
-			echo '{ "alert": "alert-danger", "message": "Your message could not been sent due to invalid reCaptcha!" }';
-			die;
-		}
-	}
+	$email = $_POST['email'];
+	$name = isset( $_POST['name'] ) ? $_POST['name'] : '';
+	$comment = isset( $_POST['comment'] ) ? $_POST['comment'] : '';
 
 	$message = '
 		<html>
@@ -83,7 +52,7 @@ if( ! empty( $_POST['email'] ) ) {
 		</html>
 		';
 
-	if( $enable_smtp == 'no' ) { // Simple Email
+	if( $enable_smtp == 'yes' ) { // Simple Email
 
 		// Always set content-type when sending HTML email
 		$headers = "MIME-Version: 1.0" . "\r\n";
@@ -91,68 +60,11 @@ if( ! empty( $_POST['email'] ) ) {
 		// More headers
 		$headers .= 'From: ' . $name . ' <' . $from . '>' . "\r\n";
 		if( mail( $receiver_email, $subject, $message, $headers ) ) {
-
-			// Redirect to success page
-			$redirect_page_url = ! empty( $_POST['redirect'] ) ? $_POST['redirect'] : '';
-			if( ! empty( $redirect_page_url ) ) {
-				header( "Location: " . $redirect_page_url );
-				exit();
-			}
-
-		   	//Success Message
-		  	echo '{ "alert": "alert-success", "message": "Your message has been sent successfully!" }';
+		  //Success Message
+		  echo '{ "alert": "alert-success", "message": "Your message has been sent successfully!" }';
 		} else {
 			//Fail Message
-		  	echo '{ "alert": "alert-danger", "message": "Your message could not been sent!" }';
-		}
-		
-	} else { // SMTP
-
-		// Email Receiver Addresses
-		$toemailaddresses = array();
-		$toemailaddresses[] = array(
-			'email' => $receiver_email, // Your Email Address
-			'name' 	=> $receiver_name // Your Name
-		);
-
-		require 'phpmailer/Exception.php';
-		require 'phpmailer/PHPMailer.php';
-		require 'phpmailer/SMTP.php';
-
-		$mail = new PHPMailer\PHPMailer\PHPMailer();
-
-		$mail->isSMTP();
-		$mail->Host     = 'smtp.titan.email '; // Your SMTP Host
-		$mail->SMTPAuth = true;
-		$mail->Username = 'hello@agentonlineweb.com'; // Your Username
-		$mail->Password = 'Kanapka12#'; // Your Password
-		$mail->SMTPSecure = 'ssl'; // Your Secure Connection
-		$mail->Port = 465; // Your Port
-		$mail->setFrom( $from, $name );
-		
-		foreach( $toemailaddresses as $toemailaddress ) {
-			$mail->AddAddress( $toemailaddress['email'], $toemailaddress['name'] );
-		}
-
-		$mail->Subject = $subject;
-		$mail->isHTML( true );
-
-		$mail->Body = $message;
-
-		if( $mail->send() ) {
-			
-			// Redirect to success page
-			$redirect_page_url = ! empty( $_POST['redirect'] ) ? $_POST['redirect'] : '';
-			if( ! empty( $redirect_page_url ) ) {
-				header( "Location: " . $redirect_page_url );
-				exit();
-			}
-
-		   	//Success Message
-		  	echo '{ "alert": "alert-success", "message": "Your message has been sent successfully!" }';
-		} else {
-			//Fail Message
-		  	echo '{ "alert": "alert-danger", "message": "Your message could not been sent!" }';
+		  echo '{ "alert": "alert-danger", "message": "Your message could not been sent!" }';
 		}
 	}
 } else {
